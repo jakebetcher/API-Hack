65914ce53f06f4f2f7fda99404a71969f4dc8ef3
Jake Betcher <jakebetcher@gmail.com>
2017-12-11 15:47:54 -0800

    retrieved and stored API data

--- app.js
+++ app.js
@@ -1,30 +1,12 @@
+const geocodingURL = "https://maps.googleapis.com/maps/api/geocode/json";
 const sygicURL = 'https://api.sygictravelapi.com/1.0/en/places/list';
-
-/*function getDataFromSygicApi(searchTerm, callback) {
-  const settings = {
-    url: sygicURL,
-    headers: {
-        "x-api-key": "1J87aob1867nkffRIHOopacsRYAJIwwLaQoghwmY"
-      },
-      data: {
-      	query: `${searchTerm}`,
-      },
-      dataType: 'json',
-      type: 'GET',
-    success: callback
-  };
-
-  $.ajax(settings);
-}*/
-
-
 
 function storeTheAddress() {
 	let theAddress = $('.js-query').val();
 	return theAddress;
 }
 
-const geocodingURL = "https://maps.googleapis.com/maps/api/geocode/json";
+
 
 function getDataFromGeocodingApi(callback) {
         let theAddress = storeTheAddress();
@@ -38,17 +20,42 @@ function getDataFromGeocodingApi(callback) {
 
  function getCoordinates(data) {
         console.log(data);
-        let latitude= data.results[0].geometry.location.lat;
-        let longitude = data.results[0].geometry.location.lng;
-        let coordinates = [latitude, longitude];
-        console.log(coordinates);
-        return coordinates;
+        const southBound = data.results[0].geometry.bounds.southwest.lat;
+        const westBound = data.results[0].geometry.bounds.southwest.lng;
+        const northBound = data.results[0].geometry.bounds.northeast.lat;
+        const eastBound = data.results[0].geometry.bounds.northeast.lng;
+        const bounds = [southBound, westBound, northBound, eastBound];
+        console.log(bounds);
+        getDataFromSygicApi(doThis, southBound, westBound, northBound, eastBound);
         }
 
-         function handleSubmit() {
+        function handleSubmit() {
           $('.js-search-form').submit( function(event) {
               getDataFromGeocodingApi(getCoordinates);
           })
       }
       
       $(handleSubmit);
+
+ function getDataFromSygicApi(callback, south, west, north, east) {
+  		const settings = {
+    	url: sygicURL,
+    	headers: {
+        "x-api-key": "1J87aob1867nkffRIHOopacsRYAJIwwLaQoghwmY"
+      	},
+      	data: {
+      		bounds: `${south}, ${west}, ${north}, ${east}`,
+      		levels: 'poi',
+      		limit: 100
+      	},
+      	dataType: 'json',
+     	type: 'GET',
+    	success: callback
+  	};
+
+  	$.ajax(settings);
+}
+
+function doThis(data) {
+	console.log(data);
+}
